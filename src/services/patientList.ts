9ba8cc6a09999d683db3ef4a7ec0b33daad2534d
NgocThom <thomd1209@gmail.com>
2025-09-15 09:22:42 +0700

    fix validate register

--- src/services/patientList.ts
+++ src/services/patientList.ts
@@ -98,8 +98,4 @@ export const getVisitHistory = async (recordId: number): Promise<VisitSummary | 
     console.error("❌ Lỗi gọi API getVisitHistory:", err);
     return null;
   }
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> origin/develop
